15d6b9b1e6b7c654b7f2e62f830246a5541ff212
Matt Mandersheid <mmandersheid@gmail.com>
2023-12-14 12:19:57 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var song
+var fft
+
+function preload () {
+  song = loadSound('swiftumz drift apart.mp3')
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  fft = new p5.FFT()
+}
+
+function draw() {
+  background(0)
+  stroke(255)
+
+  var wave = fft.waveform()
+
+  for (var i = 0; i < width; i++) {
+    var index = floor(map(1, 0, width, 0, wave.length))
+
+    var x = i
+    var y = wave[index] * 300 + height / 2
+    point(x,y)
+  }
+
+}
+
+
+function mouseClicked() {
+  if (song.isPlaying()) {
+    song.pause()
+  } else {
+    song.play()
+  }
+}
